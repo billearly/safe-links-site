3b69325fa890ec0a9e31a7ce445832814efc120f
Bill Early <earlybilly@gmail.com>
2019-05-30 22:56:37 -0400

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -55,6 +55,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link href="https://fonts.googleapis.com/css?family=Pacifico|Sacramento|Roboto&display=swap" rel="stylesheet"/>
+          <link rel="icon" href="/static/favicon.ico"></link>
         </Head>
 
         <body className="custom_class">
