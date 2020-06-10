0fafdd03a56c1dca6f6929103941442dde1a1964
Bill Early <earlybilly@gmail.com>
2020-06-09 23:02:21 -0400

    Fixing component export error

--- components/index.js
+++ components/index.js
@@ -1,7 +1,8 @@
 export { Header } from './header';
-export { Link } from './Link';
+export { Link } from './link';
 export { Results } from "./results";
 export { Spinner } from "./spinner";
 export { SearchForm, SearchInput } from "./form";
 export { Container } from "./container";
 export { Dyad } from "./dyad";
+export { TextToken } from "./text-token";
